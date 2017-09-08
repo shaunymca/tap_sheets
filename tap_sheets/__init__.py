c72bd1a6d55488644fa5d476f12ca5a0da184783
Shaun <smcavinney@rjmetrics.com>
2017-09-08 10:33:02 -0400

    fixed weird merge

--- tap_sheets/__init__.py
+++ tap_sheets/__init__.py
@@ -28,19 +28,8 @@ import httplib2
 
 from apiclient import discovery
 from oauth2client import client
-from oauth2client import tools
-from oauth2client.file import Storage
 
 LOGGER = singer.get_logger()
-
-try:
-    parser = argparse.ArgumentParser(parents=[tools.argparser])
-    parser.add_argument('-c', '--config', help='Config file', required=True)
-    parser.add_argument('-d', '--discover', help='Run in discovery mode', action='store_true')
-    flags = parser.parse_args()
-
-except ImportError:
-    flags = None
     
 import time
 
@@ -62,39 +51,14 @@ def RateLimited(maxPerSecond):
 
 # If modifying these scopes, delete your previously saved credentials
 # at ~/.credentials/sheets.googleapis.com-python-quickstart.json
-CONFIG = {
-    "scopes" : ['https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive'],
-    "client_secret_file" : 'client_secret.json',
-    "application_name" : 'Client'
-}
+CONFIG = {}
 
 def get_credentials():
-    """Gets valid user credentials from storage.
-    If nothing has been stored, or if the stored credentials are invalid,
-    the OAuth2 flow is completed to obtain the new credentials.
+    """
     Returns:
         Credentials, the obtained credential.
     """
-    
-    
-    home_dir = os.path.expanduser('~')
-    credential_dir = os.path.join(home_dir, '.credentials')
-    if not os.path.exists(credential_dir):
-        os.makedirs(credential_dir)
-    credential_path = os.path.join(credential_dir,
-                                   'sheets.googleapis.com-singer-tap.json')
-
-    store = Storage(credential_path)
-    credentials = store.get()
-    if not credentials or credentials.invalid:
-        flow = client.flow_from_clientsecrets(CONFIG['client_secret_file'], CONFIG['scopes'])
-        flow.user_agent = CONFIG['application_name']
-        if flags:
-            credentials = tools.run_flow(flow, store, flags)
-        else: # Needed only for compatibility with Python 2.6
-            credentials = tools.run(flow, store)
-        print('Storing credentials to ' + credential_path)
-    return credentials
+    return client.OAuth2Credentials(None, CONFIG['oauth_client_id'], CONFIG['oauth_client_secret'], CONFIG['refresh_token'], None, "https://www.googleapis.com/oauth2/v4/token", "stitch")
 
 def do_discover():
     discover_catalog().dump()
@@ -180,9 +144,9 @@ def do_sync(properties):
 
 def main():
     args = utils.parse_args(
-        ["scopes",
-         "client_secret_file",
-         "application_name"])
+        ["oauth_client_id",
+         "oauth_client_secret",
+         "refresh_token"])
     print(args)
     CONFIG.update(args.config)
     STATE = {}
